492e536d0c953a38bde5e299ecbbdf1e582bd290
Heritage Samuel F <30316855+Herrytheeagle@users.noreply.github.com>
2018-09-28 02:23:11 +0100

    Add files via upload

---
+++ GeneratingReturnValue.py
@@ -0,0 +1,11 @@
+from multiprocessing import Pool
+
+def job(num):
+    return num * 2
+
+if __name__ == '__main__':
+    p = Pool(processes=20)
+    data = p.map(job, range(5))
+    data2 = p.map(job, [5,2])
+    p.close()
+    print(data)

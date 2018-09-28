10eef3583086ecdfb84be59798bd56b52414e274
Heritage Samuel F <30316855+Herrytheeagle@users.noreply.github.com>
2018-09-28 02:27:38 +0100

    Add files via upload

---
+++ Multiprocessing.py
@@ -0,0 +1,11 @@
+import multiprocessing
+
+
+def spawn(num, num2):
+    print('spawned! {} {}'.format(num, num2))
+
+if __name__ == '__main__':
+    for i in range(55):
+        p = multiprocessing.Process(target=spawn, args=(i, i+1))
+        p.start()
+        p.join()
